afaf834b8230a31a6850955606e1ab6d0cbc2613
colin-rod <140830677+colin-rod@users.noreply.github.com>
2025-11-10 17:10:07 +0100

    test: add coverage for parse chat message api

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ const { compilerOptions } = require('./tsconfig.json')
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/__tests__'],
+  roots: ['<rootDir>/__tests__', '<rootDir>/apps'],
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
