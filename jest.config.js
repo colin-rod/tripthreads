2952def198daed01eed119c14e7b398ef2fc026d
colin-rod <140830677+colin-rod@users.noreply.github.com>
2025-11-10 17:10:25 +0100

    test: cover upload attachment API

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,21 @@ const { compilerOptions } = require('./tsconfig.json')
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/__tests__'],
+  roots: ['<rootDir>/__tests__', '<rootDir>/apps/web'],
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/',
   }),
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.jest.json',
+        diagnostics: {
+          ignoreCodes: [2339],
+        },
+      },
+    ],
+  },
 }
