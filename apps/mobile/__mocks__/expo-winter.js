9f3b52c5b5f49dc024d19ff5c8faccea440e26a9
Colin Rodrigues <colin.rods@gmail.com>
2025-12-08 17:21:24 +0100

    update test configs

---
+++ apps/mobile/__mocks__/expo-winter.js
@@ -0,0 +1,2 @@
+// Minimal stub to skip Expo Winter runtime during tests
+module.exports = {}
--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,3 @@
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig.json')
-
-const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths || {}, {
-  prefix: '<rootDir>/',
-})
-
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/__tests__', '<rootDir>/apps'],
-  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  setupFiles: ['<rootDir>/apps/web/jest.polyfills.cjs'],
-  setupFilesAfterEnv: ['<rootDir>/apps/web/jest.setup.cjs'],
-  moduleNameMapper: {
-    ...moduleNameMapper,
-    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-  },
-  transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.jest.json',
-        diagnostics: {
-          ignoreCodes: [2339],
-        },
-      },
-    ],
-  },
+  projects: ['<rootDir>/jest.unit.config.js', '<rootDir>/apps/mobile/jest.config.cjs'],
 }
